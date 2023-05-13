c6c273851cced3eb85bfacc69c3110eb8365f777
danielbeltrano <82431546+omardbeltrano@users.noreply.github.com>
2023-05-13 10:38:42 -0500

    Added Searcher and button, also API was implemented, and movie's img configuration

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,14 +13,55 @@
             </router-link>
         </div>
 
-        <form @submit.prevent="" class="search-box">
-            <input type="text" placeholder="What are you looking for?">
+        <form @submit.prevent="SearchMovies()" class="search-box">
+            <input type="text" placeholder="What are you looking for?" v-model="search">
+            <input type="submit" value="search">
         </form>
+
+        <div class="movies-list">
+            <div class="movie" v-for="movie in movies" :key="movie.imdbID">
+                <router-link to="'/movie/' + movie.imdbID" class="movie-link">
+                    <div class="movie-img">
+                        <img :src="movie.Poster" alt="Movie Poster"/>
+                        <div class="type">{{ movie.Type }}</div>
+                    </div>
+                </router-link>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+    import { ref } from "vue";
+    import env from "../env";
+
     export default {
+        setup(){
+            const search = ref("");
+            const movies = ref([]);
+
+            const SearchMovies = () => {
+                //Calling url with apikey and search's value
+                if (search.value != "") {
+                    fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&s=${search.value}`)
+                        .then(response => response.json())
+                        .then(data => {
+                            console.log(data);
+                            //This add the movie within the movies' array
+                            movies.value = data.Search;
+                            //This reset the searcher
+                            search.value = "";
+
+                            console.log(movies)
+                        })
+                }
+            }
+            return{
+                search,
+                movies,
+                SearchMovies
+            }
+        }
     }
 </script>
 
@@ -38,7 +79,7 @@
             position: relative;
             z-index: 0;
         }
-
+        //Feature detail
         .detail {
                 position: absolute;
                 left: 0;
@@ -57,6 +98,62 @@
                     color: #FFF;
                 }
             }
+    }
+}
+
+.search-box {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 16px;
+
+    //General inputs
+    input {
+        display: block;
+        appearance: none;
+        border: none;
+        outline: none;
+        background: none;
+
+        //Searcher
+        &[type="text"] {
+            width: 100%;
+            color: #FFF;
+            background-color: #496583;
+            font-size: 20px;
+            padding: 10px 16px;
+            border-radius: 8px;
+            margin-bottom: 15px;
+            transition: .4s;
+
+            &::placeholder{
+            color:#FFF;
+            }
+
+            &:focus {
+                box-shadow: 0px 3px 6px rgba(0,0,0,0.4);
+            }
+        }
+
+        //Div buttom
+        &[type="submit"] {
+            width: 100%;
+            max-width: 300px;
+            background-color: #42B883;
+            padding: 16px;
+            border-radius: 8px;
+            color: #FFF;
+            font-size: 20px;
+            text-transform: uppercase;
+            transition: 0.4s;
+            cursor: pointer;
+
+            &:active {
+                background-color: #3B8070;
+            }
+
+        }
     }
 }
 </style>
